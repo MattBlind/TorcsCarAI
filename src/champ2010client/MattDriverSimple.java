c45826b028857f864152609e4d3e6191bcfaa3d9
Matei Chiorescu <matt93ch@gmail.com>
2017-11-06 17:15:07 +0000

    First batch

---
+++ src/champ2010client/MattDriverSimple.java
@@ -0,0 +1,22 @@
+package champ2010client;
+
+/**
+ * Created by mattster on 06/11/17.
+ */
+public class MattDriverSimple extends Controller {
+
+    @Override
+    public Action control(SensorModel sensors) {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void shutdown() {
+
+    }
+}
